fef6cf8ef7bb1d29059e70fadf4b63e7cfc2764d
Thiago França <thiagobio.uni@gmail.com>
2025-10-19 12:01:47 -0300

    Entrega Intermediária

--- my-chrome-extension/playwright.config.ts
+++ my-chrome-extension/playwright.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig, devices } from "@playwright/test";
 import path from "node:path";
 
-const dist = path.join(__dirname, '..', 'dist');
+const dist = path.join(__dirname, 'dist'); // ou '..', dependendo da localização do dist
 
 export default defineConfig({
-    testDir: path.join(__dirname, 'tests'), // <-- aqui
-    reporter: [['list'], ['html', { outputFolder: 'playwright-report'}]],
-    projects: [
-        {
-            name:'chromium-with-extension',
-            use: {
-                ...devices['Desktop Chrome'],
-                headless: false,
-                launchOptions: {
-                    args: [
-                        `--disable-extensions-except=${dist}`,
-                        `--load-extension=${dist}`
-                    ]
-                }
-            }
+  testDir: path.join(__dirname, 'tests'), // <-- aqui deve apontar para a pasta de testes
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
+  projects: [
+    {
+      name: 'chromium-with-extension',
+      use: {
+        ...devices['Desktop Chrome'],
+        headless: false,
+        launchOptions: {
+          args: [
+            `--disable-extensions-except=${dist}`,
+            `--load-extension=${dist}`
+          ]
         }
-    ]
+      }
+    }
+  ]
 });
